360415ead8014a0201ba71fda7afa027a709cdf0
9OmP <104563841+9OmP@users.noreply.github.com>
2023-06-26 16:58:59 +0530

    Create band_name_generator_main.py

---
+++ band_name_generator_main.py
@@ -0,0 +1,11 @@
+
+print("Hello! Welcome to band Name Generator \n")
+
+city_name = input("What is the name of your home city? \n")
+
+pet_name = input("What is th name of your Pet? \n")
+
+print("The name of your band could be " + city_name + " " + pet_name)
+
+
+
